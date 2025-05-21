6ce003ab6d0cc9cf874108d1afedbfd8fda7b3fe
sovarantonia <sovarantonia@yahoo.com>
2025-05-21 21:17:02 +0300

    security config

--- src/main/java/com/example/sharesnotesapp/security/SecurityConfig.java
+++ src/main/java/com/example/sharesnotesapp/security/SecurityConfig.java
@@ -86,7 +86,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Change this to your frontend URL
+                        .allowedOrigins("http://localhost:3000", "https://share-notes-app.netlify.app/") // Change this to your frontend URL
                         .allowedMethods("*")
                         .exposedHeaders("Content-Disposition")
                         .allowedHeaders("*");
